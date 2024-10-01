fd7ab68c26b41db2414e6ee9fc4576119f42739c
AqsaIftikhar15 <165428763+AqsaIftikhar15@users.noreply.github.com>
2024-10-02 01:45:58 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,14 @@
+
+
+function Homepage() {
+  return (
+    <div className ="homeContainer">
+    <div className ="childContainer"><h4>
+      Hi! <br/>
+      I'm <b>Aqsa Iftikhar</b>,currently learning generative AI and data Science.</h4>
+    </div>
+    <div className ="childContainer2"><img src="/images/UnBjxu-removebg-preview.png" alt="profile picture" style={{ maxWidth: '100%', height: 'auto' }} /></div>
+    </div>
+  );
+}
+export default Homepage;
